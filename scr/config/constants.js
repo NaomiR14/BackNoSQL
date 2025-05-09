ebf70d3c405e51a598ec884d92bdd02435353f89
NaomiR14 <adrianaroman433@gmail.com>
2025-05-09 14:40:06 -0600

    fix: Se agrego constante de uri para la app

--- scr/config/constants.js
+++ scr/config/constants.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+export const uri = process.env.URI_APP
+
 export const port = process.env.PORT_APP
 export const mongoDomain = process.env.MONGO_DOMAIN
 export const mongoUser = process.env.MONGO_USER
