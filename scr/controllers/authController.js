f074b1c9a753224d1129b4a79d9578b656b33e83
NaomiR14 <adrianaroman433@gmail.com>
2025-05-22 20:52:08 -0600

    feat: Se agrega la logica para el login en aplicacion

--- scr/controllers/authController.js
+++ scr/controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../model/Users.js";
 import jwt from 'jsonwebtoken';
-//import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import { keyToken  } from '../config/constants.js';
 
 const generateToken = (id) => {
@@ -35,18 +35,25 @@ export const registerUser = async (req, res) => {
   }
 };
 
-// export const authenticateUser = async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email });
+//Logica 2 Nos logeamos a nuestra app
 
-//   if (user && (await bcrypt.compare(password, user.password))) {
-//     res.json({
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       token: generateToken(user._id),
-//     });
-//   } else {
-//     res.status(401).json({ message: 'Invalid email or password' });
-//   }
-// };
+export const authenticateUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  console.log("Usuario: " + user)
+
+  // console.log(`Contrasenia: ${password}   `)
+  // console.log(`Contrasenia Cifrada guardada en DB: ${user.password} `)
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401).json({ message: 'Invalid email or password' });
+  }
+};
--- scr/routes/authRoutes.js
+++ scr/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { registerUser } from '../controllers/authController.js';
+import { registerUser, authenticateUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
 //http://localhost:500/api/v0/auth/register
 router.post('/register', registerUser);
 
-///router.post('/login', authenticateUser);
+//http://localhost:500/api/v0/auth/login
+router.post('/login', authenticateUser);
 
 export default router;
